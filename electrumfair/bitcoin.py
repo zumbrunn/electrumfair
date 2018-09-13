32465deba9d7208506ddb36b96d53f6e4f22779b
Thomas König <tom@fair-coin.org>
2018-09-13 09:49:27 +0200

    fix `total amount of coins`

--- electrumfair/bitcoin.py
+++ electrumfair/bitcoin.py
@@ -38,7 +38,7 @@ from .crypto import Hash, sha256, hash_160, hmac_oneshot
 COINBASE_MATURITY = 100
 
 COIN = 100000000
-TOTAL_COIN_SUPPLY_LIMIT_IN_BTC = 21000000
+TOTAL_COIN_SUPPLY_LIMIT_IN_BTC = 53193831.467966
 
 # supported types of transaction outputs
 TYPE_ADDRESS = 0
